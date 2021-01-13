696d7d248508ed2a4114f74274cd5b68a357f983
Cody Krivacic <codykrivacic@Codys-MBP.ucsfmedicalcenter.org>
2021-01-13 12:33:23 -0800

    Using mongodb to store bins for now.

--- matcher.py
+++ matcher.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 from roseasy.utils import numeric
 from itertools import product
-import os, psutil
+import os, psutil, sys
 import pickle
 from pyrosetta import init, pose_from_file
 '''
@@ -44,11 +44,14 @@ def bin_array(array, bins):
 
 class HelixLookup(object):
 
-    def __init__(self, df, exposed_cutoff=0.5, binned_dict=None,
+    def __init__(self, df, exposed_cutoff=0.5, length_cutoff=10.8, binned_dict=None,
             binned_path=None, query_df=None):
         self.df = df
         self.df['idx'] = self.df.index
-        self.df = self.df[self.df['percent_exposed'] > exposed_cutoff]
+        if exposed_cutoff is not None:
+            self.df = self.df[self.df['percent_exposed'] > exposed_cutoff]
+        if length_cutoff is not None:
+            self.df = self.df[self.df['length'] > length_cutoff]
         self.degrees = 20
         self.angstroms = 2
         self.setup_bins()
@@ -61,6 +64,7 @@ class HelixLookup(object):
             self.query_df = query_df
             self.query_df['idx'] = self.query_df.index
             self.query_bins = self.bin_db(self.query_df)
+            print(self.query_bins)
 
     def setup_bins(self):
         nrbins = int(360//self.degrees) + 1
@@ -70,69 +74,135 @@ class HelixLookup(object):
         ntbins = int((tstop - tstart) // self.angstroms) + 1
         self.tbins = np.linspace(tstart, tstop, ntbins)
 
-    def bin_db(self, df):
+    def update_bin_db(self):
+        self.bin_db(self.df, 'helix_bins', check_name=True)
+
+    def bin_db(self, df, dbname, check_name=True):
         '''
-        Bins only need to be the lengths of the two ends of the helices
-        from one another...
+        Save parameter tells pymongo where to save (must have MongoDB
+        installed and running).
         '''
 
         from scipy.spatial.transform import Rotation as R
-        bin_size = 1
-        binned = {}
+        import pymongo
+        import subprocess
+        from pymongo import MongoClient
+        import time
+
+        client = MongoClient()
+        try:
+            client.server_info()
+        except:
+            print('Problem with MongoDB server connection; attempting to open...')
+            subprocess.Popen(['mongod', '--dbpath',
+                '/Users/codykrivacic/data/db'])
+        db = client[dbname]
+        bins = db['bins']
+        total_proteins = len(set(self.df['name']))
+        interval = 500
+
+        # import shelve
+
+        # binned = shelve.open('binned_0p3/hashtable', 'c', writeback=True)
         i = 0
-        j = 0
+        # j = 0
+        unsaved_docs = []
+        start_time = time.time()
         for name, group in df.groupby(['name']):
-            i += 1
-            if i%1000 == 0:
+            if check_name and len(list(bins.find({'name':name}))) > 0:
+                i += 1
+                continue
+
+            for combination in product(group.T.to_dict().values(),
+                    repeat=2):
+                if combination[0] != combination[1]:
+                    idx1 = combination[0]['idx']
+                    idx2 = combination[1]['idx']
+                    transform = numeric.Transformation(combination[0]['vector'],
+                            combination[1]['vector'])
+                    rot = R.from_matrix(transform.rotation)
+                    rot = rot.as_euler('xyz', degrees=True)
+                    # combination[0]['rotation'] = rot
+                    # combination[0]['translation'] = transform.translation
+
+                    rbin = bin_array(rot, self.rbins)
+                    tbin = bin_array(transform.translation, self.tbins)
+                    rbin2 = bin_array(rot, self.rbins + (self.degrees/2))
+                    tbin2 = bin_array(transform.translation, self.tbins +
+                            (self.angstroms/2))
+
+                    x = [tbin[0], tbin2[0]]
+                    y = [tbin[1], tbin2[1]]
+                    z = [tbin[2], tbin2[2]]
+                    phi = [rbin[0], rbin2[0]]
+                    psi = [rbin[1], rbin2[1]]
+                    om = [rbin[2], rbin2[2]]
+
+                    for bin_12 in product(x, y, z, phi, psi,
+                        om):
+                        bin_12 = ' '.join(map(str, bin_12))
+                        doc = {
+                                'bin':bin_12, 
+                                'name': name,
+                                'idx1':idx1,
+                                'idx2':idx2
+                        }
+                        unsaved_docs.append(doc)
+                        # bins.insert_one(doc)
+                        # if bin_12 not in binned:
+                            # binned[bin_12] = {}
+                        # if name not in binned[bin_12]:
+                            # binned[bin_12][name] = []
+                        # binned[bin_12][name].append((idx1, idx2))
+            if i%interval == 0:
                 # For large databases, dump dictionaries when memory
                 # usage goes over 6 GB. Only use for forming initial lookup
                 # database.
                 mem_used = psutil.Process(os.getpid()).memory_info().rss
-                print('{} PDBs processed so far.'.format(i))
+                print('{} of {} PDBs processed so far.'.format(
+                    i, total_proteins))
                 print('Currently using {} G of memory'.format(
                     mem_used * 10**-9
                     ))
+                print('Saving to db...')
+                bins.insert_many(unsaved_docs, ordered=False)
+                bins.create_index([('bin', 'hashed')])
+                elapsed = time.time() - start_time
+                rate = interval / elapsed
+                remaining = (total_proteins - i) / rate / 3600
+                print('Done. 500 pdbs took {} seconds. Est. {} h remaining'.format(
+                    elapsed, remaining
+                    ))
+                start_time = time.time()
+                unsaved_docs = []
+                # print('Syncing db...')
+                # binned.sync()
+                # print('Done.')
                 # If more than 6G memory used, dump just in case.
-                if mem_used > 6 * 10**9:
-                    out = "binned/{}.pkl".format(j)
-                    j += 1
-                    with open(out, 'wb') as f:
-                        pickle.dump(binned, f)
-                    del binned
-                    binned = {}
+                # if mem_used > 12 * 10**9:
+                    # out = "binned_0p3/{}.pkl".format(j)
+                    # j += 1
+                    # with open(out, 'wb') as f:
+                        # pickle.dump(binned, f)
+                    # del binned
+                    # binned = {}
 
-            for combination in product(group.T.to_dict().values(),
-                    repeat=2):
-                idx1 = combination[0]['idx']
-                idx2 = combination[1]['idx']
-                transform = numeric.Transformation(combination[0]['vector'],
-                        combination[1]['vector'])
-                rot = R.from_matrix(transform.rotation)
-                rot = rot.as_euler('xyz', degrees=True)
-                # combination[0]['rotation'] = rot
-                # combination[0]['translation'] = transform.translation
+        print('Saving to db...')
+        bins.insert_many(unsaved_docs, ordered=False)
+        bins.create_index([('bin', 'hashed')])
 
-                rbin = bin_array(rot, self.rbins)
-                tbin = bin_array(transform.translation, self.tbins)
-                if (tbin, rbin) not in binned:
-                    binned[(tbin, rbin)] = {}
-                if name not in binned[(tbin, rbin)]:
-                    binned[(tbin, rbin)][name] = []
-                binned[(tbin,
-                    rbin)][name].append((idx1, idx2))
 
-                rbin = bin_array(rot, self.rbins + (self.degrees/2))
-                tbin = bin_array(transform.translation, self.tbins +
-                        (self.angstroms/2))
+    def score_match(self, list_of_index_pairs):
+        """
+        Idea (idk where else to put this):
+            To get 3rd, 4th, etc. helices, do a reverse lookup. That is,
+            for each bin in the FOUND PDB, look for matches in the QUERY
+            pdb.
+        """
+        for tup in list_of_index_pairs:
+            query_row = self.query_df.loc[tup[0]]
+            db_row = self.df.loc[tupe[1]]
 
-                if (tbin, rbin) not in binned:
-                    binned[(tbin, rbin)] = {}
-                if name not in binned[(tbin, rbin)]:
-                    binned[(tbin, rbin)][name] = []
-                binned[(tbin,
-                    rbin)][name].append((idx1, idx2))
-
-        return binned
 
 
     def match(self):
@@ -142,7 +212,11 @@ class HelixLookup(object):
             for name in self.query_bins[key]:
                 try:
                     print(self.binned[key])
+                    for name in self.binned[key]:
+                        print(name)
+                        print(self.binned[key][name])
                 except:
+                    print('BIN NOT IN DICT: {}'.format(key))
                     print(self.query_bins[key])
 
 
@@ -151,21 +225,33 @@ def test():
 
     test_path = 'test_files/6r9d.cif'
     init()
-    pose = pose_from_file(test_path)
+    pose = pose_from_file(test_path).split_by_chain(1)
+    print(pose.size())
     scanner = scan_helices.PoseScanner(pose)
     helices = scanner.scan_pose_helices()
     helices = pd.DataFrame(helices)
-    helices = helices[helices['percent_exposed'] > 0.5]
+    print(helices)
+    helices = helices[helices['percent_exposed'] > 0.25]
     print(helices)
 
     lookup = HelixLookup(pd.read_pickle('dataframes/final.pkl'),
-            binned_path='binned/final.pkl', query_df=helices)
+            binned_path='binned_0p3/final.pkl', query_df=helices)
     lookup.match()
-    # lookup.bin_db()
-    # out = "binned/last.pkl"
+
+
+def make_hash_table():
+    print('Loading database and setting up lookup object...')
+    # length cutoff of 2 turns or 10.8 angstroms
+    lookup = HelixLookup(pd.read_pickle('dataframes/final.pkl'),
+            exposed_cutoff=0.3, length_cutoff=10.8)
+    print('Done.')
+    # binned = lookup.bin_db(lookup.df)
+    lookup.update_bin_db()
+    # out = "binned_0p3/last.pkl"
     # with open(out, 'wb') as f:
-        # pickle.dump(lookup.binned, f)
+        # pickle.dump(binned, f)
 
 
 if __name__=='__main__':
-    test()
+    # test()
+    make_hash_table()
