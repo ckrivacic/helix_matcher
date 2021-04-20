775119b40a3f4356ee347e6a902bc7acd33cf606
Cody Krivacic <codykrivacic@Codys-MacBook-Pro.local>
2021-04-20 15:08:15 -0700

    submit rifgen made more generalizable (not tested)

--- rifdock/submit_rifgen.sh
+++ rifdock/submit_rifgen.sh
@@ -1,5 +1,6 @@
 export PYTHON_PATH=/wynton/home/kortemme/krivacic/software/anaconda3/bin/python
-export RUNDIR=/wynton/home/kortemme/krivacic/intelligent_design/helix_matcher/test_files/boundary/
-qsub -h -cwd -o logs/ -e errs/ -t 107 -l h_rt=6:00:00 -l mem_free=6G -b y \
+export RUNDIR=$1
+export num_tasks=$(echo $(ls $RUNDIR | grep patch | wc -l))
+qsub -h -cwd -o logs/ -e errs/ -t $num_tasks -l h_rt=6:00:00 -l mem_free=6G -b y \
 -N rifgen $PYTHON_PATH \
 run_rifgen.py $RUNDIR --sge
