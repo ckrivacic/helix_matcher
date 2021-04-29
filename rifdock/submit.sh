045aa182df067c29924e0126d24a732430d1cb76
Cody Krivacic <codykrivacic@Codys-MBP.ucsf.edu>
2021-04-29 15:47:47 -0700

    submit cluster run; cluster script renamed to avoid confusion with clustering outputs

---
+++ rifdock/submit.sh
@@ -0,0 +1,6 @@
+export PYTHON_PATH=/wynton/home/kortemme/krivacic/software/anaconda3/bin/python
+export RUNDIR=$1
+export num_tasks=$(echo $(ls $RUNDIR | grep patch | wc -l))
+qsub -cwd -o logs/ -e logs/ -t 1-$num_tasks -l h_rt=6:00:00 -l mem_free=6G -l scratch=200M -b y \
+-N rifdock_full $PYTHON_PATH \
+cluster_run.py $RUNDIR --sge
