55837a51bf4091501e22b718d9a64956270a121e
ckrivacic <krivacic@berkeley.edu>
2021-04-30 09:46:33 -0700

    realpath

--- rifdock/submit.sh
+++ rifdock/submit.sh
@@ -1,6 +1,6 @@
 export PYTHON_PATH=/wynton/home/kortemme/krivacic/software/anaconda3/bin/python
 export RUNDIR=$1
-export SCAFFOLD=$2
+export SCAFFOLD=(realpath $2)
 export num_tasks=$(echo $(ls $RUNDIR | grep patch | wc -l))
 qsub -cwd -o logs/ -t 1-$num_tasks -l h_rt=6:00:00 -l mem_free=6G -l scratch=2G -j y -b y \
 -N rifdock_full $PYTHON_PATH \
