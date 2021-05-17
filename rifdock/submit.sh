4ef2d19c7a50112a4d1b07e075ff8dcebbfa0e68
Cody Krivacic <codykrivacic@Codys-MacBook-Pro.local>
2021-05-17 13:01:39 -0700

    make log dir

--- rifdock/submit.sh
+++ rifdock/submit.sh
@@ -2,6 +2,7 @@ export PYTHON_PATH=/wynton/home/kortemme/krivacic/software/anaconda3/bin/python
 export RUNDIR=$1
 export SCAFFOLD=$(realpath $2)
 export num_tasks=$(echo $(ls $RUNDIR | grep patch | wc -l))
+mkdir $RUNDIR/logs/
 qsub -cwd -o $RUNDIR/logs/ -t 1-$num_tasks -l h_rt=6:00:00 -l mem_free=6G -l scratch=2G -j y -b y \
 -N rifdock_full $PYTHON_PATH \
 run_sge.py $RUNDIR $SCAFFOLD --sge
