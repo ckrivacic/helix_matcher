f7277abc170d3fdbc8482f477bed18ca39db0573
Cody Krivacic <codykrivacic@Codys-MBP.ucsf.edu>
2021-05-17 14:07:29 -0700

    check for existence of logdir

--- rifdock/submit.sh
+++ rifdock/submit.sh
@@ -2,7 +2,10 @@ export PYTHON_PATH=/wynton/home/kortemme/krivacic/software/anaconda3/bin/python
 export RUNDIR=$1
 export SCAFFOLD=$(realpath $2)
 export num_tasks=$(echo $(ls $RUNDIR | grep patch | wc -l))
-mkdir $RUNDIR/logs/
+if [ ! -d $RUNDIR/logs ]
+then
+    mkdir $RUNDIR/logs/
+fi
 qsub -cwd -o $RUNDIR/logs/ -t 1-$num_tasks -l h_rt=6:00:00 -l mem_free=6G -l scratch=2G -j y -b y \
 -N rifdock_full $PYTHON_PATH \
 run_sge.py $RUNDIR $SCAFFOLD --sge
