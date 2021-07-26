cbab4b07eff8135fef797062e19635fc83ee1f4f
Cody Krivacic <codykrivacic@Codys-MacBook-Pro.local>
2021-07-26 15:54:56 -0700

    scan hnelix command

--- helix/commands/scan_pdb_folder.py
+++ helix/commands/scan_pdb_folder.py
@@ -16,6 +16,9 @@ Options:
 '''
 import docopt
 from helix import workspace as ws
+from copy import deepcopy
+from helix.utils import utils
+from helix import big_jobs
 
 
 def main():
@@ -23,8 +26,42 @@ def main():
     workspace = ws.workspace_from_dir(args['<workspace>'])
     pdffolder = args['<pdbfolder>']
 
+    script_path = \
+            os.path.join(os.path.abspath(
+                os.path.dirname(os.path.realpath(__file__))), '..',
+            'matching', 'scan_pdb_folder.py')
+    if not os.path.exists(script_path):
+        raise Exception("Error: {} does not exist.".format(script_path))
+
     if args['--sge']:
         args['--local'] = False
+    if args['--clear']:
+        workspace.clear_database
+
+    cmd = workspace.python_path, script_path
+    argpass = ['--recursive', '--ntasks', '--split-chains']
+
+
+    if args['--local']:
+        if args['--task']:
+            local_cmd = deepcopy(cmd)
+            for arg in argpass:
+                local_cmd += arg, args[arg]
+            utils.run_command(local_cmd)
+        else:
+            for n in range(1, args['--ntasks'] + 1):
+                local_cmd= deepcopy(cmd)
+                local_cmd += '--task', str(n)
+                utils.run_command(local_cmd)
+
+    else:
         script_name = 'scan_helices'
-        if args['--clear']:
-            workspace.clear_database
+        print('Submitting the following command to SGE:')
+        print(' '.join(cmd))
+        big_jobs.submit(
+                workspace, cmd, nustruct=ntasks,
+                max_runtime=args['--max-runtime'],
+                max_memory=args['--max-memory'],
+                test_run=False,
+                create_job_info=False
+                )
