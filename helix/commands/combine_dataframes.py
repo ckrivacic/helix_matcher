e8d3c480a85a7d74119d24a862e930d7acf05004
Cody Krivacic <codykrivacic@Codys-MBP.ucsfmedicalcenter.org>
2021-07-29 11:46:47 -0700

    main

--- helix/commands/combine_dataframes.py
+++ helix/commands/combine_dataframes.py
@@ -17,14 +17,15 @@ import sys
 import os
 
 
-args = docopt.docopt(__doc__)
-out = pd.DataFrame()
-for path in sorted(glob.glob(args['<folder>'] + '/*.pkl')):
-    print('Reading {}'.format(path))
-    df = pd.read_pickle(path)
-    out = out.append(df, ignore_index=True)
+def main():
+    args = docopt.docopt(__doc__)
+    out = pd.DataFrame()
+    for path in sorted(glob.glob(args['<folder>'] + '/*.pkl')):
+        print('Reading {}'.format(path))
+        df = pd.read_pickle(path)
+        out = out.append(df, ignore_index=True)
 
-if args['--out']:
-    out.to_pickle(args['--out'])
-else:
-    out.to_pickle(args['<folder>'] + '/final.pkl')
+    if args['--out']:
+        out.to_pickle(args['--out'])
+    else:
+        out.to_pickle(args['<folder>'] + '/final.pkl')
