c8c0942708ba5bd12d71d6ee8858e78daaf43e94
Cody Krivacic <codykrivacic@Codys-MBP.ucsfmedicalcenter.org>
2021-08-20 16:24:33 -0700

    ignore unrecognized

--- helix/commands/01_prep_rifdock.py
+++ helix/commands/01_prep_rifdock.py
@@ -135,7 +135,7 @@ def test_3n2n():
 
 def main():
     args = docopt.docopt(__doc__)
-    init()
+    init('--ignore_unrecognized_res')
     root_workspace = ws.workspace_from_dir(args['<workspace>'])
     # posefile = os.path.abspath(args['<target_pdb>'])
     if args['--target']:
