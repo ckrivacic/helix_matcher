a76e54d58716a04243be2b657171dd5812dbe5e8
Cody Krivacic <codykrivacic@Codys-MacBook-Pro.local>
2021-09-28 13:52:59 -0700

    fastdesign of patchman outputs

---
+++ helix/commands/design_patchman.py
@@ -0,0 +1,75 @@
+"""
+Run FastDesign + FlexPepDock on PatchMAN outputs.
+
+Usage:
+    helix design_patchman <workspace> [options]
+
+Options:
+    --local, -l  Run locally
+    --target=PDB, -t  Only run for a sepcific target if multiple exist
+    --make-dirs  Just make the directories and stop.
+    --test-run  Mark as test run. Does nothing for now.
+    --task=INT  Only run a specific task
+    --clear, -o  Overwrite a prevoius run. Gets rid of docked outputs,
+    log files, and job info files.
+    --flexpepdock  Run flexpepdock at the end of the PatchMAN run
+    --relax  Run relax on target
+"""
+import helix.workspace as ws
+import os
+import docopt
+from helix.utils import utils
+from helix import submit
+from copy import deepcopy
+
+
+def main():
+    args = docopt.docopt(__doc__)
+    # sizes = [10, 14, 21, 28]
+    workspace = ws.workspace_from_dir(args['<workspace>'])
+    script_path = os.path.join(
+            os.path.dirname(os.path.realpath(__file__)),
+            '..', 'patchman', 'design_patchman.py'
+            )
+    if not os.path.exists(script_path):
+        raise Exception("Error: {} does not exist.".format(script_path))
+    if args['--target']:
+        targets = [workspace.target_rifdock_path(args['--target'])]
+    else:
+        targets = workspace.all_rifdock_workspaces
+
+    for target in targets:
+        rif_workspace = ws.RIFWorkspace(workspace.root_dir, target)
+        # inputs = rif_workspace.unclaimed_inputs
+        inputs = sorted(glob.glob(
+            os.path.join(rif_workspace.focus_dir, 'patch_*',
+                workspace.scaffold_prefix + '*', 'docked_full', '*.pdb.gz')
+            ))
+        ntasks = len(inputs)
+
+        cmd = workspace.python_path, script_path
+        cmd += target,
+
+        if args['--flexpepdock']:
+            cmd += '--flexpepdock'
+
+        if args['--relax']:
+            cmd += '--relax'
+
+        if args['--task']:
+            cmd += '--task', args['--task']
+            ntasks = 1
+
+        if args['--local']:
+            print('Runinng locally')
+            for n in range(1, ntasks + 1):
+                local_cmd = deepcopy(cmd)
+                if not args['--task']:
+                    local_cmd += '--task', str(n)
+                utils.run_command(local_cmd)
+
+        else:
+            print('Submitting jobs for {}'.format(target))
+            submit.submit(rif_workspace, cmd, distributor='sge',
+                    make_dirs=args['--make-dirs'],
+                    test_run=args['--test-run'], clear=args['--clear'],)
---
+++ helix/patchman/design_patchman.py
@@ -0,0 +1,97 @@
+from pyrosetta.rosetta.core.pack.task import TaskFactory
+from pyrosetta.rosetta.core.pack.task import operation
+from pyrosetta.rosetta.core.select import residue_selector
+from pyrosetta import pose_from_pdb
+from pyrosetta import create_score_function
+import pyrosetta
+from distutils.dir_util import copy_tree
+import docopt
+import os, sys, glob
+from helix import workspace as ws
+from helix.utils import utils
+from helix import big_jobs
+
+
+def strlist_to_vector1_str(strlist):
+    # Should also go in utils eventually
+    from pyrosetta.rosetta.utility import vector1_std_string
+    vector = vector1_std_string()
+    for string in strlist:
+        vector.append(string)
+    return vector
+
+
+def main():
+    args = docopt.docopt(__doc__)
+    workspace, job_info = big_jobs.initiate()
+    if not hasattr(workspace, 'docking_directory'):
+        raise Exception("Error: run_sge.py requires RIFWorkspaces as input. You "\
+                "may have provided the root directory to this script "\
+                "somehow.")
+
+    total_jobs = len(glob.glob(workspace.focus_dir + '/patch_*/len_*'))
+    print('TOTAL JOBS: {}'.format(total_jobs))
+    num_tasks = total_jobs
+
+    task_id = job_info['task_id']
+    if not task_id:
+        if args['--task']:
+            task_id = int(args['--task']) - 1
+        else:
+            task_id = 0
+
+    inputs = sorted(glob.glob(
+        os.path.join(workspace.focus_dir, 'patch_*',
+            workspace.scaffold_prefix + '*', 'docked_full', '*.pdb.gz')
+        ))
+
+    print('TASK: {}'.format(task_id))
+    pdb = inputs[task_id]
+    target = workspace.target_path_clean
+    os.system('ls ???_????_*_*.pdb > input_list')
+    # inputs = []
+    # with open('input_list', 'r') as f:
+        # for line in f:
+            # inputs.append(line.strip())
+    # for pdb in inputs:
+    pose = pose_from_pdb(pdb)
+    ref = create_score_function('ref2015')
+    fastdes = pyrosetta.rosetta.protocols.denovo_designs.movers.FastDesign(ref)
+
+    selector = residue_selector.ChainSelector('B')
+    not_selector = residue_selector.NotResidueSelector(selector)
+    tf = TaskFactory()
+    no_packing = operation.PreventRepackingRLT()
+    static = operation.OperateOnResidueSubset(no_packing,
+            not_selector)
+    notaa = operation.ProhibitSpecifiedBaseResidueTypes(
+            strlist_to_vector1_str(['GLY']),
+            selector)
+    tf.push_back(static)
+    tf.push_back(notaa)
+    packertask = tf.create_task_and_apply_taskoperations(pose)
+    print('REPACK')
+    print(packertask.repacking_residues())
+    print('DESIGN')
+    print(packertask.designing_residues())
+
+    fastdes.set_task_factory(tf)
+    fastdes.set_scorefxn(ref)
+    fastdes.apply(pose)
+    score = ref(pose)
+    pose.dump_pdb(pdb)
+
+    exe = os.path.join(
+            workspace.rosetta_dir, 'source', 'bin',
+            'FlexPepDocking.{}'.format(suffix)
+            )
+    if not os.path.exists('docked_full/'):
+        os.path.makedirs('docked_full', exist_ok=True)
+    cmd = [exe, '-in:file:s', pdb, '-scorefile',
+            'score.sc',
+            '-out:pdb_gz', '-lowres_preoptimize',
+            '-flexPepDocking:pep_refine',
+            '-flexPepDocking:flexpep_score_only', '-ex1', '-ex2aro',
+            '-use_input_sc', 'unboundrot', target]
+            # '-out:prefix', 'docked_full/',
+    utils.run_command(cmd)
