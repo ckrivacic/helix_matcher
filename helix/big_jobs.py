fc619697b004f6d8f88170b0dea152032f1e3f45
Cody Krivacic <codykrivacic@Codys-MBP.ucsfmedicalcenter.org>
2021-10-06 15:18:29 -0700

    more troubleshooting

--- helix/big_jobs.py
+++ helix/big_jobs.py
@@ -141,6 +141,7 @@ def submit(workspace, cmd, create_job_info=True, **params):
     for param in cmd:
         qsub_command += param,
 
+    print(' '.join(qsub_command))
     status = process.check_output(qsub_command).decode('utf-8')
     status_pattern = re.compile(r'Your job-array (\d+).[0-9:-]+ \(".*"\) has been submitted')
     status_match = status_pattern.match(status)
