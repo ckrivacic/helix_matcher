fff5eb5ceb0161b8b27e83ad2725da6d4f8cdd5e
Cody Krivacic <codykrivacic@Codys-MacBook-Pro.local>
2022-01-25 00:12:30 -0800

    print pose size

--- helix/benchmark/score_pdb.py
+++ helix/benchmark/score_pdb.py
@@ -49,6 +49,8 @@ class PDBInterface(object):
             minmover = XmlObjects.static_get_mover(minmover_str)
             minmover.apply(self.pose)
             print('MINMOVER APPLIED')
+        print('POSE SIZE:')
+        print(self.pose.size())
         self.ss_str = Dssp(self.pose).get_dssp_secstruct()
         self.dist = dist
         if pdbid:
