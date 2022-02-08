87c35cf5b4df2843936c07b0b531e78be720fbc9
Cody Krivacic <codykrivacic@Codys-MacBook-Pro.local.ucsf.edu>
2022-02-07 19:26:53 -0800

    there we goo; not local

--- helix/patchman/design_patchman.py
+++ helix/patchman/design_patchman.py
@@ -172,7 +172,7 @@ def main():
         final_inputs = []
         if args['--suffix']:
             for inp in temp_inputs:
-                if not args['--sufix'] in os.path.basename(inp):
+                if not args['--suffix'] in os.path.basename(inp):
                     print('{} not designed, appending'.format(inp),
                             flush=True)
                     final_inputs.append(inp)
