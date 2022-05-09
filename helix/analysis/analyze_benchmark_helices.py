1d507a42feb6a0a8578c63d72e6e582da063d01a
Cody Krivacic <codykrivacic@Codys-MacBook-Pro.local.ucsf.edu>
2022-05-08 22:30:19 -0700

    analyze benchmark structures

--- helix/analysis/analyze_benchmark_helices.py
+++ helix/analysis/analyze_benchmark_helices.py
@@ -32,7 +32,7 @@ def make_bench_helix_pose(pose, row, length):
         this_target = target.clone()
         print('THIS TARGET: ', this_target)
         print('TARGET SIZE: ', this_target.size())
-        rosetta.core.pose.append_subpose_to_pose(this_target, pose_clone, i, this_stop)
+        rosetta.core.pose.append_subpose_to_pose(this_target, pose_clone, i, this_stop, True)
         print('THIS POSE: ', this_target)
         print('THIS POSE SIZE: ', this_target.size())
         poses.append(this_target)
