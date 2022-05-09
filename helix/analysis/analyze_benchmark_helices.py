5de0cfb45fa49037da06272ddfcaa24002a77846
Cody Krivacic <codykrivacic@Codys-MacBook-Pro.local.ucsf.edu>
2022-05-08 21:57:24 -0700

    analyze benchmark structures

--- helix/analysis/analyze_benchmark_helices.py
+++ helix/analysis/analyze_benchmark_helices.py
@@ -49,6 +49,7 @@ def main():
         for length in [14, 28]:
             initial_pose = utils.pose_from_wynton(row['name'])
             poses = make_bench_helix_pose(row['name'], row, length)
+            print(poses)
             for pose in poses:
                 row = analyze_structures.analyze_pose(pose, row['target'], row['chain'])
                 row['length'] = f'len_{length}'
